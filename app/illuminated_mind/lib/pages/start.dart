5085e0cb905ceec4c75e9f5bd298f67c44cc812b
Nadine Krietenbrink <nkrietenbrink@inovex.de>
2019-04-24 21:43:22 +0200

    add some bubbles

--- app/illuminated_mind/lib/pages/start.dart
+++ app/illuminated_mind/lib/pages/start.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/material.dart';
 import 'runes.dart';
+import 'speechBubble.dart';
 
-class StartPage extends StatelessWidget {
+class StartPage extends StatefulWidget {
+  _StartState createState() => _StartState();
+}
+
+class _StartState extends State<StartPage> {
+  final List<String> bubbleStrings = [
+    "Willkommen zu deiner Prüfung",
+    "Du bist ein Lehrling der Zauberkünste und möchtest deinen Abschluss erlangen.",
+    "bla bla bla... Du kannst jetzt starten"
+  ];
+
+  int counter = 0;
+  bool buttonText = false;
+
+  void handleBubbleText() {
+    if (counter == (bubbleStrings.length - 2)) {
+      setState(() {
+        buttonText = true;
+        counter++;
+      });
+    } else if (counter < (bubbleStrings.length - 1)) {
+      setState(() {
+        counter++;
+      });
+    } else {
+      Navigator.push<bool>(
+        context,
+        MaterialPageRoute(builder: (BuildContext context) => RunesPage()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-          Text(
-            "StartScreen",
-            textScaleFactor: 2,
-          ),
-          Text("Meister heißt Lehrling willkommen"),
+          SpeechBubbleWidget(bubbleStrings[counter]),
           RaisedButton(
-            child: Text("Los gehts"),
-            onPressed: () => Navigator.push<bool>(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext context) => RunesPage()),
-                ),
+            child: Text(buttonText ? "Los gehts" : "Weiter"),
+            onPressed: () => handleBubbleText(),
           )
         ]),
       ),
